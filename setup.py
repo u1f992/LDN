ecbb0c08657ffd5551964ef85e0d2ecbff020af5
Yannik Marchand <ymarchand@me.com>
2026-02-13 17:36:19 +0100

    Push new changes to PyPI

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ long_description = \
 
 setuptools.setup(
     name = "ldn",
-    version = "0.0.15",
+    version = "0.0.16",
     description = description,
     long_description = long_description,
     author = "Yannik Marchand",
